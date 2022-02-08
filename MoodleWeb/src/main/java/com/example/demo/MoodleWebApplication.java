b83783a134cff778c6b0eaa7d224079fc15befde
Nikola Obradović <obradovicnikola009@gmail.com>
2022-02-08 21:41:55 +0100

    added async support for emails

--- MoodleWeb/src/main/java/com/example/demo/MoodleWebApplication.java
+++ MoodleWeb/src/main/java/com/example/demo/MoodleWebApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import com.example.demo.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableAsync
 @EntityScan("model")
 @EnableConfigurationProperties({ FileStorageProperties.class })
 public class MoodleWebApplication {
--- MoodleWeb/src/main/java/com/example/demo/controller/email/EmailServiceImpl.java
+++ MoodleWeb/src/main/java/com/example/demo/controller/email/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.controller.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,15 +13,15 @@ public class EmailServiceImpl {
 	@Autowired
 	private JavaMailSender emailSender;
 
-	public void sendSimpleMessage(
-      String to, String subject, String text) {
+	@Async
+	public void sendSimpleMessage(String to, String subject, String text) {
 
-		SimpleMailMessage message = new SimpleMailMessage(); 
-        message.setFrom("tmprisprojekat2022@gmail.com");
-        message.setTo(to); 
-        message.setSubject(subject); 
-        message.setText(text);
-        emailSender.send(message);
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("tmprisprojekat2022@gmail.com");
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(text);
+		emailSender.send(message);
 
 	}
 }
